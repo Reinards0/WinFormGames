ec1ce2c9435b260b30ca5a28024a91b65a4ccbf3
Reinards0 <115609593+Reinards0@users.noreply.github.com>
2024-11-24 14:42:01 +0700

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Snake
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Snake));
+            this.pictureBoard = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoard
+            // 
+            this.pictureBoard.Location = new System.Drawing.Point(5, 5);
+            this.pictureBoard.Name = "pictureBoard";
+            this.pictureBoard.Size = new System.Drawing.Size(560, 560);
+            this.pictureBoard.TabIndex = 0;
+            this.pictureBoard.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "wall.png");
+            this.imageList1.Images.SetKeyName(1, "bonus1.png");
+            this.imageList1.Images.SetKeyName(2, "bonus2.png");
+            this.imageList1.Images.SetKeyName(3, "bonus3.png");
+            this.imageList1.Images.SetKeyName(4, "bonus4.png");
+            this.imageList1.Images.SetKeyName(5, "snake_body.png");
+            this.imageList1.Images.SetKeyName(6, "snake_head.png");
+            // 
+            // Snake
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(572, 573);
+            this.Controls.Add(this.pictureBoard);
+            this.Name = "Snake";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Snake_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BtnDn);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoard)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoard;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Snake : Form
+    {
+        Random rand;
+        enum boardfields
+        {
+            Free,
+            Snak,
+            Food
+        };
+        enum controls
+        {
+            Up,
+            Dn,
+            Lt,
+            Rt
+        }
+        struct SCoords
+        {
+            public int x, y;
+        }
+        boardfields[,] field;
+        SCoords[] SnXY;
+        int sLen;
+        controls mov;
+        Graphics gfx;
+        public Snake()
+        {
+            InitializeComponent();
+            field = new boardfields[16, 16];
+            SnXY = new SCoords[196];
+            rand = new Random();
+        }
+        private void Snake_Load(object sender, EventArgs e)
+        {
+            this.Text = "Classic Snake";
+            pictureBoard.Image = new Bitmap(560, 560);
+            gfx = Graphics.FromImage(pictureBoard.Image);
+            gfx.Clear(Color.White);
+            for(int i = 0; i <= 15; i++)
+            {   // top & bottom walls
+                gfx.DrawImage(imageList1.Images[0], i * 35, 0);
+                gfx.DrawImage(imageList1.Images[0], i * 35, 525);
+            }
+            for (int i = 1; i <= 14; i++)
+            {   // left & right walls
+                gfx.DrawImage(imageList1.Images[0], 0, i * 35);
+                gfx.DrawImage(imageList1.Images[0], 525, i * 35);
+            }
+            // Init the snake
+            SnXY[0].x = 2;
+            SnXY[0].y = 7;
+            SnXY[1].x = 2;
+            SnXY[1].y = 8;
+            SnXY[2].x = 2;
+            SnXY[2].y = 9;
+            gfx.DrawImage(imageList1.Images[6], 70, 7 * 35);
+            gfx.DrawImage(imageList1.Images[5], 70, 8 * 35);
+            gfx.DrawImage(imageList1.Images[5], 70, 9 * 35);
+            field[2, 7] = boardfields.Snak;
+            field[2, 8] = boardfields.Snak;
+            field[2, 9] = boardfields.Snak;
+            mov = controls.Up;
+            sLen = 3;
+            for (int i = 0; i < 7; i++)
+            {
+                Foods();
+            }
+        }
+        private void Foods()
+        {
+            int x, y;
+            var p = rand.Next(1, 5);
+            do
+            {
+                x = rand.Next(1, 14);
+                y = rand.Next(1, 14);
+            } while (field[x, y] != boardfields.Free);
+            field[x, y] = boardfields.Food;
+            gfx.DrawImage(imageList1.Images[p], x * 35, y * 35);
+        }
+
+        private void BtnDn(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    if (mov != controls.Dn)
+                        mov = controls.Up;
+                    break;
+                case Keys.Down:
+                    if (mov != controls.Up)
+                        mov = controls.Dn;
+                    break;
+                case Keys.Left:
+                    if (mov != controls.Rt)
+                        mov = controls.Lt;
+                    break;
+                case Keys.Right:
+                    if (mov != controls.Lt)
+                        mov = controls.Rt;
+                    break;
+            }
+        }
+        private void EndGame()
+        {
+            timer1.Enabled = false;
+            MessageBox.Show("Game Over!");
+            this.Text = "Game Over";
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            gfx.FillRectangle(Brushes.White, SnXY[sLen - 1].x * 35, SnXY[sLen - 1].y * 35, 35, 35);
+            field[SnXY[sLen - 1].x, SnXY[sLen - 1].y] = boardfields.Free;
+            pictureBoard.Refresh();
+            for(int i = sLen; i >= 1; i--) // move the snake pieces
+            {
+                SnXY[i].x = SnXY[i - 1].x;
+                SnXY[i].y = SnXY[i - 1].y;
+            }
+            gfx.DrawImage(imageList1.Images[5], SnXY[0].x * 35, SnXY[0].y * 35);
+            //pictureBoard.Refresh();
+            switch (mov) // direction change
+            {
+                case controls.Up:
+                    SnXY[0].y--;
+                    break;
+                case controls.Dn:
+                    SnXY[0].y++;
+                    break;
+                case controls.Lt:
+                    SnXY[0].x--;
+                    break;
+                case controls.Rt:
+                    SnXY[0].x++;
+                    break;
+            }
+            if (SnXY[0].x < 1 || SnXY[0].x > 14 || SnXY[0].y < 1 || SnXY[0].y > 14) // wall collision
+            {
+                EndGame();
+                pictureBoard.Refresh();
+                return;
+            }
+            if (field[SnXY[0].x, SnXY[0].y] == boardfields.Snak) // self collision
+            {
+                EndGame();
+                pictureBoard.Refresh();
+                return;
+            }
+            if (field[SnXY[0].x, SnXY[0].y] == boardfields.Food) // eat food
+            {
+                gfx.DrawImage(imageList1.Images[5], SnXY[sLen].x * 35, SnXY[sLen].y * 35);
+                field[SnXY[sLen].x, SnXY[sLen].y] = boardfields.Snak;
+                sLen++;
+                if (sLen < 190) Foods();
+                else
+                {
+                    timer1.Enabled = false;
+                    this.Text = "Game Complete";
+                    return;
+                }
+                this.Text = "Classic Snake (" + (sLen - 3) + ")";
+            }
+            gfx.DrawImage(imageList1.Images[6], SnXY[0].x * 35, SnXY[0].y * 35);
+            field[SnXY[0].x, SnXY[0].y] = boardfields.Snak;
+            pictureBoard.Refresh();
+        }
+    }
+}
